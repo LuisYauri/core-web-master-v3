70299eaf69ecda2037dbcccb40a9f143756184e7
Luis Yauri <luis.yauri1@unmsm.edu.pe>
2019-07-26 00:48:17 -0500

    update

--- src/app/modules/administrator/entertainments/entertainments.component.ts
+++ src/app/modules/administrator/entertainments/entertainments.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {EntertainmentsService} from '../../../services/administrator/entertainments.service';
 import {editDataModel, entertainmentsModel, sendDataAddModel} from '../../../models/administrator/entertainments.modal';
-import Swal from "sweetalert2";
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-entertainments',
@@ -10,7 +10,7 @@ import Swal from "sweetalert2";
 })
 export class EntertainmentsComponent implements OnInit {
 
-  pageActual:number = 1;
+  actualPage: number = 1;
 
   itemsHeader = {title: 'Entretenimientos', sub_title: 'core'};
 
@@ -19,13 +19,13 @@ export class EntertainmentsComponent implements OnInit {
   listEntertainments: entertainmentsModel[];
 
   //DataAddModal
-  titleAddModal = 'COMENZAR REGISTRO DE CUENTA';
-  sendDataAddModal: sendDataAddModel = {name:'',username:'',password:''};
+  titleAddModal = 'COMENZAR EL REGISTRO DE UNA NUEVA CUENTA';
+  sendDataAddModal: sendDataAddModel = {name: '', username: '', password: ''};
 
   //DataEditModal
   titleEditModal = 'COMENZAR A EDITAR ESTA CUENTA';
-  dataModal: entertainmentsModel = {id:0,name:'',username:'',password:'',created_at:'',updated_at:''}
-  dataEditModal: editDataModel = {name:'',username:'',password:''};
+  dataEditModal: editDataModel = {name: '', username: '', password: ''};
+  idDataEdit: number;
 
   constructor(private entertainmentsService: EntertainmentsService) {
   }
@@ -35,25 +35,29 @@ export class EntertainmentsComponent implements OnInit {
   }
 
   async setEditAccount() {
-    // @ts-ignore
-    jQuery('#editModal').modal('hide');
-    try{
-      const response = await this.entertainmentsService.editAccount(this.dataModal.id.toString(),this.dataEditModal).toPromise();
-      console.log(this.dataEditModal);
-      this.sweetAlertConfirm("Actualizado correctamente.");
-      this.getListEntertainments();
-    }catch (e) {
-      console.log(e)
+    if(this.ckeckData(this.dataEditModal)){
+      // @ts-ignore
+      jQuery('#editModal').modal('hide');
+      try {
+        const response = await this.entertainmentsService.editAccount(this.idDataEdit.toString(), this.dataEditModal).toPromise();
+        console.log(this.dataEditModal);
+        this.sweetAlertConfirm('Actualizado correctamente.');
+        this.getListEntertainments();
+      } catch (e) {
+        console.log(e);
+      }
+    }else{
+      this.sweetAlertLackOfData('Falta completar todos los campos.');
     }
     this.clearVariables();
   }
 
   async deleteAccount(id: number) {
-    try{
+    try {
       const response = await this.entertainmentsService.deleteAccount(id.toString()).toPromise();
-      this.sweetAlertConfirm("Eliminado correctamente.");
+      this.sweetAlertConfirm('Eliminado correctamente.');
       this.getListEntertainments();
-    }catch (e) {
+    } catch (e) {
       console.log(e);
     }
   }
@@ -67,23 +71,32 @@ export class EntertainmentsComponent implements OnInit {
     }
   }
 
-  async setAddAccount(){
-    // @ts-ignore
-    jQuery('#addModal').modal('hide');
-    try{
-      const response = await this.entertainmentsService.setAddAccount(this.sendDataAddModal).toPromise();
-      this.sendDataAddModal = {name:'',username:'',password:''};
-      this.sweetAlertConfirm("Registrado correctamente.");
-      this.getListEntertainments();
-    }catch (e) {
-      console.log(e);
+  async setAddAccount() {
+    if (this.ckeckData(this.sendDataAddModal)) {
+      // @ts-ignore
+      jQuery('#addModal').modal('hide');
+      try {
+        const response = await this.entertainmentsService.setAddAccount(this.sendDataAddModal).toPromise();
+        this.sendDataAddModal = {name: '', username: '', password: ''};
+        this.sweetAlertConfirm('Registrado correctamente.');
+        this.getListEntertainments();
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      this.sweetAlertLackOfData('Falta completar todos los campos.');
     }
+    this.clearVariables();
   }
 
-  sweetAlertQuestion(id: number){
+  ckeckData(user:sendDataAddModel){
+    return user.name.length != 0 && user.username.length != 0 && user.password.length != 0
+  }
+
+  sweetAlertQuestion(id: number) {
     Swal.fire({
       title: '¿Estas seguro de eliminarlo?',
-      text: "Se eliminará la información de la lista.",
+      text: 'Se eliminará la información de la lista.',
       type: 'warning',
       showCancelButton: true,
       confirmButtonColor: '#3085d6',
@@ -94,10 +107,10 @@ export class EntertainmentsComponent implements OnInit {
       if (result.value) {
         this.deleteAccount(id);
       }
-    })
+    });
   }
 
-  sweetAlertConfirm(text:string){
+  sweetAlertConfirm(text: string) {
     Swal.fire({
       type: 'success',
       title: text,
@@ -107,14 +120,25 @@ export class EntertainmentsComponent implements OnInit {
   }
 
   editAccount(account: entertainmentsModel) {
-    this.dataModal  = account;
+    this.idDataEdit = account.id;
     this.dataEditModal.name = account.name;
     this.dataEditModal.username = account.username;
     this.dataEditModal.password = account.password;
   }
 
-  clearVariables(){
-    this.dataModal= {id:0,name:'',username:'',password:'',created_at:'',updated_at:''}
-    this.dataEditModal = {name:'',username:'',password:''};
+  clearVariables() {
+    this.idDataEdit = -1;
+    this.dataEditModal = {name: '', username: '', password: ''};
+    this.sendDataAddModal = {name: '', username: '', password: ''};
+  }
+
+  sweetAlertLackOfData(text: string) {
+    Swal.fire({
+      type: 'warning',
+      title: text,
+      showConfirmButton: false,
+      timer: 1500
+    });
   }
 }
+
